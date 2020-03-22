df00026d73295d71dbccbf651a55d28eb5f68d9a
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2020-03-22 20:51:47 +0200

    Add: delete post

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -13,6 +13,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = []
   pSub: Subscription
+  dSub: Subscription
   searchStr = ''
 
   constructor(private postsService: PostsService) { }
@@ -24,13 +25,17 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   }
 
   remove(id: string) {
-
+    this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
   }
 
   ngOnDestroy() {
     if (this.pSub) {
       this.pSub.unsubscribe()
     }
+    if (this.dSub) {
+      this.dSub.unsubscribe()
+    }
   }
-
 }
--- src/app/shared/services/posts.service.ts
+++ src/app/shared/services/posts.service.ts
@@ -31,4 +31,8 @@ export class PostsService {
          }))
       }))
    }
+
+   remove(id: string): Observable<void> {
+      return this.http.delete<void>(`${environment.dbBaseUrl}/posts/${id}.json`)
+   }
 }
